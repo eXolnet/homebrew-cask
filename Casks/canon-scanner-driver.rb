e46ae0e654e7e00d5ef6e748bb153cf7e47209b8
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2018-12-17 12:12:00 -0500

    Add canon-scanner-driver latest

---
+++ Casks/canon-scanner-driver.rb
@@ -0,0 +1,10 @@
+cask 'canon-scanner-driver' do
+  version '2.13.0'
+  sha256 'e9d00025aec26385cf22fcbd22ef4f1a7720267f938902aafc11822545bb4924'
+
+  url 'http://gdlp01.c-wss.com/gds/9/0100009299/05/mac-scan-v2130-02.dmg'
+  name 'Canon Scanner Driver & Utilities'
+  homepage 'http://www.canon.ca/en/Contact-Support/Consumer/Downloads'
+
+  pkg 'Canon_ScanGear_MF.pkg'
+end
