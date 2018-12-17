b82f35b41860493130678bffc8f67d96e6439351
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2018-12-17 11:20:38 -0500

    Add shoretel-connect latest

---
+++ Casks/shoretel-connect.rb
@@ -0,0 +1,10 @@
+cask 'shoretel-connect' do
+  version '213.100.2885.0'
+  sha256 '7d72d1c1b173753d652b216bc92db29bfbb46a6c113c166202009fb26068b40f'
+
+  url 'http://10.0.2.10/ShorewareResources/ClientInstall/ShoreTelConnect.dmg'
+  name 'ShoreTel Connect'
+  homepage 'http://10.0.2.10/ShorewareResources/ClientInstall/'
+
+  app 'ShoreTel Connect.app'
+end
