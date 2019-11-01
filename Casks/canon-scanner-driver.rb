372b5a3a02b60f2ba2389d781526fb6c47c9568d
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2019-11-01 14:53:53 -0400

    Update canon-scanner-driver to 2.15.1

--- Casks/canon-scanner-driver.rb
+++ Casks/canon-scanner-driver.rb
@@ -1,8 +1,8 @@
 cask 'canon-scanner-driver' do
-  version '2.13.0'
+  version '2.15.1'
   sha256 'e9d00025aec26385cf22fcbd22ef4f1a7720267f938902aafc11822545bb4924'
 
-  url 'http://gdlp01.c-wss.com/gds/9/0100009299/05/mac-scan-v2130-02.dmg'
+  url 'http://gdlp01.c-wss.com/gds/3/0100010193/07/mac-scan-v2151-00.dmg'
   name 'Canon Scanner Driver & Utilities'
   homepage 'http://www.canon.ca/en/Contact-Support/Consumer/Downloads'
 
