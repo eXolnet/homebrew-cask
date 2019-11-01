6b33a3eabf5648eb0850b81524908c82a6d1903e
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2019-11-01 14:53:35 -0400

    Update canon-printer-driver to 10.11.3

--- Casks/canon-printer-driver.rb
+++ Casks/canon-printer-driver.rb
@@ -1,8 +1,8 @@
 cask 'canon-printer-driver' do
-  version '10.11.1'
+  version '10.11.3'
   sha256 'da6c9cbd85a75754061053b0cd0086f0297ea2f9d52a2bb0056fff1438bcff12'
 
-  url 'http://gdlp01.c-wss.com/gds/2/0100009332/05/mac-mf-v10111-01.dmg'
+  url 'http://gdlp01.c-wss.com/gds/3/0100010433/03/mac-mf-v10113-00.dmg'
   name 'Canon MF Printer Driver & Utilities'
   homepage 'http://www.canon.ca/en/Contact-Support/Consumer/Downloads'
 
