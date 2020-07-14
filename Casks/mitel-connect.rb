bc04c1887c4162a1e88ac59debef28c5c818c317
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2020-07-14 09:39:43 -0400

    Update mitel-connect to 214.100.1222.0

--- Casks/mitel-connect.rb
+++ Casks/mitel-connect.rb
@@ -1,6 +1,6 @@
 cask 'mitel-connect' do
-  version '214.100.1217.0'
-  sha256 '474c0f7cf56f98eb567ecafbf2b236238a3aa25358162e49ec12ffeb3d3a539f'
+  version '214.100.1222.0'
+  sha256 '6c5f571d1c5a5898ca367a96f43676d44772504d2c48dca8b6359d4f48db0bb9'
 
   url 'http://10.0.2.10/ShorewareResources/ClientInstall/MitelConnect.dmg'
   name 'Mitel Connect'
