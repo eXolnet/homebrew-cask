3872fbb96900a85c3e4964c40259563872c4d37a
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2021-02-09 13:07:39 -0500

    Update canon-scanner-driver to 2.15.6

--- Casks/canon-scanner-driver.rb
+++ Casks/canon-scanner-driver.rb
@@ -1,8 +1,8 @@
 cask 'canon-scanner-driver' do
-  version '2.15.2'
-  sha256 '07384f45c7ac5c20535705b1c81e97fa1f96412edec5e56458e5c8213844e471'
+  version '2.15.6'
+  sha256 '5efcaeccd8b1f1103d2bb966199a47fc50f0116996d987bd51178d61474c2151'
 
-  url 'https://gdlp01.c-wss.com/gds/9/0100010619/02/mac-scan-v2152-00.dmg'
+  url 'https://gdlp01.c-wss.com/gds/9/0100010619/06/mac-scan-v2156-00.dmg'
   name 'Canon Scanner Driver & Utilities'
   homepage 'http://www.canon.ca/en/Contact-Support/Consumer/Downloads'
 
