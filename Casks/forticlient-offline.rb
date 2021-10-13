bb52897627a10bf13ac4438ec25ff046ef985cf0
Pat Gagnon-Renaud <pgrenaud@exolnet.com>
2021-10-13 16:48:08 -0400

    Export/import settings when installing/uninstalling forticlient-offline

--- Casks/forticlient-offline.rb
+++ Casks/forticlient-offline.rb
@@ -8,6 +8,28 @@ cask 'forticlient-offline' do
 
   pkg 'Install.mpkg'
 
+  postflight do
+    next unless File.exist?("/Library/Application Support/Fortinet/FortiClient_settings.xml")
+
+    system_command "/Library/Application Support/Fortinet/FortiClient/bin/fcconfig",
+                   args: [
+                     "-o", "import",
+                     "-m", "all",
+                     "-f", "/Library/Application Support/Fortinet/FortiClient_settings.xml",
+                   ],
+                   sudo: true
+  end
+
+  uninstall_preflight do
+    system_command "/Library/Application Support/Fortinet/FortiClient/bin/fcconfig",
+                   args: [
+                     "-o", "export",
+                     "-m", "all",
+                     "-f", "/Library/Application Support/Fortinet/FortiClient_settings.xml",
+                   ],
+                   sudo: true
+  end
+
   uninstall script: {
       executable: '/Applications/FortiClientUninstaller.app/Contents/MacOS/Uninstall',
   }
