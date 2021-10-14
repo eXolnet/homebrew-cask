d3a4c0e043534aa7c4912207a5af6504eea7b8fc
Pat Gagnon-Renaud <pgrenaud@exolnet.com>
2021-10-14 17:33:35 -0400

    Update mitel-connect to 214.100.1265.0

--- Casks/mitel-connect.rb
+++ Casks/mitel-connect.rb
@@ -1,6 +1,6 @@
 cask 'mitel-connect' do
-  version '214.100.1222.0'
-  sha256 '6c5f571d1c5a5898ca367a96f43676d44772504d2c48dca8b6359d4f48db0bb9'
+  version '214.100.1265.0'
+  sha256 'edde4530115d02c8c9315914539ea48252ba080c10de5d2e2846077f19b16e5f'
 
   url 'http://10.0.2.10/ShorewareResources/ClientInstall/MitelConnect.dmg'
   name 'Mitel Connect'
