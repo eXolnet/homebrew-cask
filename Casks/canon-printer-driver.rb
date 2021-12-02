8393fb7b46e85ed84eb0576742376eb0b2f7f3f0
Pat Gagnon-Renaud <pgrenaud@exolnet.com>
2021-12-02 12:31:55 -0500

    Update canon-printer-driver to 10.11.8

--- Casks/canon-printer-driver.rb
+++ Casks/canon-printer-driver.rb
@@ -1,8 +1,8 @@
 cask 'canon-printer-driver' do
-  version '10.11.6'
-  sha256 'cded9cbfc4ad45f03258066fc96b3a7c2ee0f4e486043813b9242e3fd568c078'
+  version '10.11.8'
+  sha256 'd5f10278e0dd91a898757299adb4a566e5f14e6ad073c885da268768b7b8172e'
 
-  url 'https://gdlp01.c-wss.com/gds/4/0100010874/03/mac-mf-v10116-00.dmg'
+  url 'https://gdlp01.c-wss.com/gds/4/0100011244/02/mac-mf-v10118-00.dmg'
   name 'Canon MF Printer Driver & Utilities'
   homepage 'http://www.canon.ca/en/Contact-Support/Consumer/Downloads'
 
