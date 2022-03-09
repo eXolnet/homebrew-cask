c48e231701ef7cf52835738627323f45c275b909
Pat Gagnon-Renaud <pgrenaud@exolnet.com>
2022-03-09 17:08:01 -0500

    Add 3cx-exolnet latest

---
+++ Casks/3cx-exolnet.rb
@@ -0,0 +1,10 @@
+cask '3cx-exolnet' do
+  version '18.8.508'
+  sha256 '74bf2366fc596353ebf637bdf67c67aa331f38ded60bfc62cc0e28afaff46afe'
+
+  url 'https://exolnet.clients.alt380.ca:4643/webclient/api/app/mac'
+  name '3CX Desktop App'
+  homepage 'https://exolnet.clients.alt380.ca:4643/webclient/'
+
+  app '3CX Desktop App.app'
+end
