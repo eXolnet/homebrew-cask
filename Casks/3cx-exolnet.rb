be392646fc0e714fffd76ab2632ec5ccc423000d
Patricia Gagnon-Renaud <pgrenaud@exolnet.com>
2023-02-07 15:20:44 -0500

    Update 3cx-exolnet to 18.11.1213

--- Casks/3cx-exolnet.rb
+++ Casks/3cx-exolnet.rb
@@ -1,6 +1,6 @@
 cask '3cx-exolnet' do
-  version '18.10.461'
-  sha256 '418ba9926bd3beb305dc18310f7c192479cf460936c056fe4492d6f2f060d700'
+  version '18.11.1213'
+  sha256 '5407cda7d3a75e7b1e030b1f33337a56f293578ffa8b3ae19c671051ed314290'
 
   url 'https://exolnet.clients.alt380.ca:4643/webclient/api/app/mac'
   name '3CX Desktop App'
