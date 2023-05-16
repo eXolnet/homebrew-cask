bd4100c3d41eaae854e3aa18c5f41c6678540c4e
Patricia Gagnon-Renaud <pgrenaud@exolnet.com>
2023-05-16 10:09:08 -0400

    Fix 3cx-exolnet to ensure unique url

--- Casks/3cx-exolnet.rb
+++ Casks/3cx-exolnet.rb
@@ -2,7 +2,7 @@ cask '3cx-exolnet' do
   version '18.12.425'
   sha256 'a90c93e8f47ebfc86f6764cbcdf0c2ad52fefd76e1575d99d41c749101ebd1de'
 
-  url 'https://exolnet.clients.alt380.ca:4643/webclient/api/app/mac'
+  url "https://exolnet.clients.alt380.ca:4643/webclient/api/app/mac##{version}"
   name '3CX Desktop App'
   homepage 'https://exolnet.clients.alt380.ca:4643/webclient/'
 
