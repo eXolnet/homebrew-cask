97461c3c07dccbf09ae53c65e155a42a34dd3e01
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2023-09-11 12:07:59 -0400

    feat: update 3cx-exolnet to 18.13.959

--- Casks/3cx-exolnet.rb
+++ Casks/3cx-exolnet.rb
@@ -1,6 +1,6 @@
 cask '3cx-exolnet' do
-  version '18.13.949'
-  sha256 'f715de1c7a4fcd23a8f604349b43bbe4145227f94468aca01ec9b31e80748c0e'
+  version '18.13.959'
+  sha256 '53e7443d8b1b212ff1441d934331894c99f08fc90224f5558a959e1a077efeee'
 
   url "https://exolnet.clients.alt380.ca:4643/webclient/api/app/mac##{version}"
   name '3CX Desktop App'
