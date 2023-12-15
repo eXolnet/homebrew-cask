b78b56315e7015038a17e7bfca4e3de9b9a170d1
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2023-12-15 12:02:34 -0500

    Update Canon Scanner driver to v2.15.14

--- Casks/canon-scanner-driver.rb
+++ Casks/canon-scanner-driver.rb
@@ -1,8 +1,8 @@
 cask 'canon-scanner-driver' do
-  version '2.15.10'
-  sha256 '0485a03ef39ecfdf93f0d306d06db70162ec51ee6101b8b216a6bf379343eb35'
+  version '2.15.14'
+  sha256 'cb41803f7db3dba7467715a8b0eb892c101e0c4c2dd082f285dfd820d8f432a3'
 
-  url 'https://gdlp01.c-wss.com/gds/3/0100011373/02/mac-scan-v21510-00.dmg'
+  url 'https://gdlp01.c-wss.com/gds/3/0100011783/04/mac-scan-v21514-00.dmg'
   name 'Canon Scanner Driver & Utilities'
   homepage 'https://canoncanada.custhelp.com/app/home'
 
